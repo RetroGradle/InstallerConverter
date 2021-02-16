c1f4fd9ec3cb8f02d9cf6ccac2cec349fefb0146
sciwhiz12 <arnoldnunag12@gmail.com>
2021-02-17 02:52:37 +0800

    Make dedicated CachingException for errors during caching

--- src/main/java/uk/gemwire/installerconverter/util/Caching.java
+++ src/main/java/uk/gemwire/installerconverter/util/Caching.java
@@ -8,21 +8,37 @@ public interface Caching {
 
     Path CACHE = Path.of(".cache");
 
-    static Path cached(String path, IOConsumer<Path> generator) throws IOException {
-        Files.createDirectories(CACHE);
+    static Path cached(String path, IOConsumer<Path> generator) throws CachingException {
+        try {
+            Files.createDirectories(CACHE);
+        } catch (IOException exception) {
+            throw new CachingException("Failed to create parent directories for cached file " + path, exception);
+        }
         return cached(CACHE.resolve(path), generator);
     }
 
-    static Path cached(Path cached, IOConsumer<Path> generator) throws IOException {
+    static Path cached(Path cached, IOConsumer<Path> generator) throws CachingException {
         if (Files.exists(cached)) return cached;
 
         System.out.printf("Caching: no cached copy of %s, running generator%n", cached);
-        generator.accept(cached);
+        try {
+            generator.accept(cached);
+        } catch (IOException exception) {
+            throw new CachingException("Failed to generate file " + cached, exception);
+        }
 
-        if (!Files.exists(cached)) throw new AssertionError("Generator did not generate the cached file");
+        if (!Files.exists(cached)) throw new CachingException("Generator did not generate file" + cached);
 
         return cached;
     }
 
+    class CachingException extends RuntimeException {
+        public CachingException(String message) {
+            super(message);
+        }
 
+        public CachingException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
 }
