5683989a07d8a2a5450c103cdef7c446ec19e2b9
AterAnimAvis <AterAnimAvis@gmail.com>
2021-02-17 16:17:04 +0000

    fix: Id Conversion again
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/main/java/uk/gemwire/installerconverter/v1_5/Conversions.java
+++ src/main/java/uk/gemwire/installerconverter/v1_5/Conversions.java
@@ -9,10 +9,14 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 public abstract class Conversions {
 
     public static String convertId(String id) throws IllegalArgumentException {
-        String[] parts = id.split("-", 3);
+        String[] parts = id.split("-");
 
-        if (parts.length != 3) throw new IllegalArgumentException("Invalid id length: " + id);
+        if (parts.length < 3 || parts.length > 4) throw new IllegalArgumentException("Invalid id length: " + id);
         if(!parts[1].toLowerCase(Locale.ROOT).startsWith("forge")) throw new IllegalArgumentException("Invalid id: " + id);
+
+        if (Objects.equals(parts[0], parts[2])) {
+            return parts[0] + "-forge-" + parts[1].toLowerCase(Locale.ROOT).replaceFirst("forge", "");
+        }
 
         return parts[0] + "-forge-" + parts[2];
     }
--- src/test/java/uk/gemwire/installerconverter/v1_5/ConversionsTest.java
+++ src/test/java/uk/gemwire/installerconverter/v1_5/ConversionsTest.java
@@ -11,6 +11,11 @@ public class ConversionsTest {
     void convertId() {
         assertEquals("a-forge-b", Conversions.convertId("a-forgeextra-b"));
         assertEquals("1.12.2-forge-14.23.5.2847", Conversions.convertId("1.12.2-forge1.12.2-14.23.5.2847"));
+        assertEquals("1.11.2-forge-13.20.1.2588", Conversions.convertId("1.11.2-forge1.11.2-13.20.1.2588"));
+        assertEquals("1.10.2-forge-12.18.3.2511", Conversions.convertId("1.10.2-forge1.10.2-12.18.3.2511"));
+        assertEquals("1.9.4-forge-12.17.0.2051", Conversions.convertId("1.9.4-forge1.9.4-12.17.0.2051"));
+        assertEquals("1.8.9-forge-11.15.1.2318", Conversions.convertId("1.8.9-forge1.8.9-11.15.1.2318-1.8.9"));
+        assertEquals("1.7.10-forge-10.13.4.1614", Conversions.convertId("1.7.10-Forge10.13.4.1614-1.7.10"));
 
         assertThrows(IllegalArgumentException.class, () -> Conversions.convertId(""));
         assertThrows(IllegalArgumentException.class, () -> Conversions.convertId("a-b"));
