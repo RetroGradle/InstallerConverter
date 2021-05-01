3711f242c2c15bc97f2537bef31cc306bdd09cca
AterAnimAvis <AterAnimAvis@gmail.com>
2021-05-01 15:50:07 +0100

    wip: refactor keyStoreFile -> keystore
    Signed-off-by: AterAnimAvis <AterAnimAvis@gmail.com>

--- src/main/java/uk/gemwire/installerconverter/util/signing/JarSignerInterop.java
+++ src/main/java/uk/gemwire/installerconverter/util/signing/JarSignerInterop.java
@@ -6,11 +6,11 @@ public class JarSignerInterop {
 
     //TODO: Actually implement the logic this uses here so we don't have to do this reflection
     /** @see sun.security.tools.jarsigner.Main */
-    public static void sign(File keyStoreFile, String storepass, String keypass, String alias, File input, File output) {
+    public static void sign(File keystore, String storepass, String keypass, String alias, File input, File output) {
         try {
             Class.forName("sun.security.tools.jarsigner.Main").getDeclaredMethod("main", String[].class).invoke(null,
                 (Object) new String[] {
-                    "-keystore", keyStoreFile.getAbsolutePath(),
+                    "-keystore", keystore.getAbsolutePath(),
                     "-storepass", storepass,
                     "-keypass", keypass,
                     "-signedjar", output.getAbsolutePath(),
